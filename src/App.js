e95edf4010a995a4efe7495d9a5bfb41fe8cf743
SeoSiun <siun0816@hanyang.ac.kr>
2021-09-13 20:41:06 +0900

    netflix

--- src/App.js
+++ src/App.js
@@ -12,39 +12,39 @@ function App(){
   const [page, setPage] = useState(1);
 
   const handleButtonClick = (dir)=>{
-    console.log(dir);
     alert(dir);
     if(dir==="next") setPage(page+1);
     else if(dir==="prev") setPage(page-1);
-  }
+  };
   const handleMenuClick = (menu)=>{
-    console.log(menu)
     alert(menu);
-  }
-  const test = () =>{
-    console.log("뭐여 ㅅㅂ");
-    alert("뭐냐고 ㅅㅂ~~");
-  }
+  };
+
+  const GitCrawler = require('./gitCrawler'); 
 
   return(
-    <div id="root" className="App">
-      <User className="User App-user" name="Jacob Lee" src={userImg} auth="Admin"/>
-      <div className="App-buttons">
-        <Button className="Button-prev" value="prev" onClick={handleButtonClick}/>
-        <Button className="Button-next" value="next" onClick={handleButtonClick}/>
+    <div id="root">
+      GitCrawler
+      <div className="App">
+        <h1>{page}</h1>
+
+        <User className="User App-user" name="Jacob Lee" src={userImg} auth="Admin"/>
+        <div className="App-buttons">
+          <Button className="Button-prev" value="prev" onClick={handleButtonClick}/>
+          <Button className="Button-next" value="next" onClick={handleButtonClick}/>
+        </div>
+        <img className="App-logo" src={logo} alt="logo"/>
+        <div className="App-movie">
+          <Movie className="Movie" title="The Crown" star={4}/>
+        </div>
+        <Menu className="App-menu1 Menu" value="Play" season={4} episode={10} onClick={handleMenuClick}/>
+        <Menu className="App-menu2 Menu" value="All" onClick={handleMenuClick}/>
+        <Menu className="App-menu3 Menu" value="Audio" onClick={handleMenuClick}/>
+        <Menu className="App-menu4 Menu" value="Remove" onClick={handleMenuClick}/>
+        <div className="App-movieImg">
+          <img className="Movie-img" src={movieImg} alt="Movie" /> 
+        </div>
       </div>
-      <img className="App-logo" src={logo} alt="logo"/>
-      <div className="App-movie">
-        <Movie className="Movie" title="The Crown" star={4}/>
-      </div>
-      <Menu className="App-menu1 Menu" value="Play" season={4} episode={10} onClick={handleMenuClick}/>
-      <Menu className="App-menu2 Menu" value="All" onClick={handleMenuClick}/>
-      <Menu className="App-menu3 Menu" value="Audio" onClick={handleMenuClick}/>
-      <Menu className="App-menu4 Menu" value="Remove" onClick={handleMenuClick}/>
-      <div className="App-movieImg">
-        <img className="Movie-img" src={movieImg} alt="Movie" /> 
-      </div>
-      <button onClick={test}>ㅠㅠㅠㅠㅠㅠ</button>
     </div>
   );
 }
